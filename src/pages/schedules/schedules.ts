0937a6bfd601597854d520d1f569757f44ab96a5
Svetlana Korbut <tebba@yandex.ru>
2023-02-07 00:10:15 +0100

    feat:background-video to Schedules Page

--- src/pages/schedules/schedules.ts
+++ src/pages/schedules/schedules.ts
@@ -1,5 +1,7 @@
 import Page from "../../core/templates/page";
 import './schedules.css'
+import { renderBackground } from "../../core/components/background-video";
+import Video from '../../assets/video/hoyenelparque_cabecera-video.mov'
 
 class SchedulesPage extends Page {
     static TextObject = {
@@ -11,12 +13,16 @@ class SchedulesPage extends Page {
     }
 
     render() {
-        const title: HTMLHeadingElement = this.createTitle(SchedulesPage.TextObject.MainTitle);
+        const tiles = document.createElement('div');
+        tiles.classList.add("tile");
+        const tilesLayout = `
+        <div class="tile_left">AQUARIUM TODAY</div>
+        <div class="tile_right">Check the activity schedules for today.
+        <br>On Sunday, 5th febrary, Oceanogràfic hosts the final of the stage of La Volta a la Comunitat Valenciana.
+        <br>Throughout the day, we will have free activities in the access square. For this reason, the aquarium parking will remain closed, but you can park your car in the vicinity of the enclosure or come by public transport.</div>`;
 
-        const background = document.createElement('div');
-        background.classList.add('abc')
-
-        this.container.append(title, background)
+        tiles.innerHTML = tilesLayout;
+        this.container.append(renderBackground(Video), tiles);
         return this.container;
     }
 }
