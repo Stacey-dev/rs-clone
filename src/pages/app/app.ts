8c2dca8a03cf90e0aa0be797a31518a3294e9bef
Svetlana Korbut <tebba@yandex.ru>
2023-02-12 12:58:50 +0100

    fix:conservation page

--- src/pages/app/app.ts
+++ src/pages/app/app.ts
@@ -21,65 +21,65 @@ export const enum PageIds {
 }
 
 class App {
-  private static container: HTMLElement = document.body;
-  private static defaultPageId: string = 'current-page';
-  initialPage: PricesPage;
-  private header: Header;
-  private footer: Footer;
-  static main: Main;
+    private static container: HTMLElement = document.body;
+    private static defaultPageId: string = 'current-page';
+    initialPage: PricesPage;
+    private header: Header;
+    private footer: Footer;
+    static main: Main;
 
-  static renderNewPage(idPage: string): void {
-    const currentPageHTML: Element | null = document.querySelector(`#${App.defaultPageId}`);
-    if (currentPageHTML) {
-      currentPageHTML.remove();
+    static renderNewPage(idPage: string): void {
+        const currentPageHTML: Element | null = document.querySelector(`#${App.defaultPageId}`);
+        if (currentPageHTML) {
+            currentPageHTML.remove();
+        }
+
+        let page: Page | null = null;
+
+        const splittedHash: string[] = window.location.hash.slice(1).split('?');
+        const path: string = splittedHash[0];
+
+        if (idPage === PageIds.PricesPage || path === '' || path.includes(PageIds.PricesPage)) {
+            page = new PricesPage(idPage);
+        } else if (idPage === PageIds.SchedulesPage || path.includes(PageIds.SchedulesPage)) {
+            page = new SchedulesPage(idPage);
+        } else if (path.includes(PageIds.QuizPage) || path.includes(PageIds.QuizPage)) {
+            page = new QuizPage(idPage);
+        } else if (path.includes(PageIds.CiutatPage) || path.includes(PageIds.CiutatPage)) {
+            page = new CiutatPage(idPage);
+        } else if (path.includes(PageIds.TicketPage) || path.includes(PageIds.TicketPage)) {
+            page = new TicketPage(idPage);
+        } else {
+            page = new ErrorPage(idPage, ErrorTypes.Error_404);
+        }
+
+        if (page) {
+            const pageHTML = page.render();
+            pageHTML.id = App.defaultPageId;
+            App.main.appendNewPage(pageHTML);
+        }
     }
 
-    let page: Page | null = null;
-
-    const splittedHash: string[] = window.location.hash.slice(1).split('?');
-    const path: string = splittedHash[0];
-
-    if (idPage === PageIds.PricesPage || path === '' || path.includes(PageIds.PricesPage)) {
-      page = new PricesPage(idPage);
-    } else if (idPage === PageIds.SchedulesPage || path.includes(PageIds.SchedulesPage)) {
-      page = new SchedulesPage(idPage);
-    } else if (path.includes(PageIds.ConservationPage) || path.includes(PageIds.ConservationPage)) {
-      page = new ConservationPage(idPage);
-    } else if (path.includes(PageIds.CiutatPage) || path.includes(PageIds.CiutatPage)) {
-      page = new CiutatPage(idPage);
-    } else if (path.includes(PageIds.TicketPage) || path.includes(PageIds.TicketPage)) {
-      page = new TicketPage(idPage);
-    } else {
-      page = new ErrorPage(idPage, ErrorTypes.Error_404);
+    private enableRouteChange(): void {
+        window.addEventListener('hashchange', () => {
+            const hash = window.location.hash.slice(1);
+            App.renderNewPage(hash);
+        });
     }
 
-    if (page) {
-      const pageHTML = page.render();
-      pageHTML.id = App.defaultPageId;
-      App.main.appendNewPage(pageHTML);
+    constructor() {
+        this.initialPage = new PricesPage('prices-calendar');
+        this.header = new Header('header', 'header');
+        this.footer = new Footer('footer', 'footer');
+        App.main = new Main('main', 'main');
     }
-  }
 
-  private enableRouteChange(): void {
-    window.addEventListener('hashchange', () => {
-      const hash = window.location.hash.slice(1);
-      App.renderNewPage(hash);
-    });
-  }
-
-  constructor() {
-    this.initialPage = new PricesPage('prices-calendar');
-    this.header = new Header('header', 'header');
-    this.footer = new Footer('footer', 'footer');
-    App.main = new Main('main', 'main');
-  }
-
-  run(): void {
-    this.enableRouteChange();
-    App.container.append(this.header.render());
-    App.container.append(App.main.render());
-    App.renderNewPage('/');
-    App.container.append(this.footer.render());
-  }
+    run(): void {
+        this.enableRouteChange();
+        App.container.append(this.header.render());
+        App.container.append(App.main.render());
+        App.renderNewPage('/');
+        App.container.append(this.footer.render());
+    }
 }
 export default App;
