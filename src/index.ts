8c904e5ed0bdf7685c6e57f9eaac22e8943b5ca5
Anastasia <nastia.shrai@gmail.com>
2023-03-01 23:46:05 +0200

    fix: correct styles in header

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,5 @@
 import App from './pages/app/app';
-import './style.css'
+import './style.css';
 
 const app: App = new App();
 app.run();
-
-alert("Уважаемые проверяющие! Очень просим проверить нашу работу в четверг! Большое спасибо!")
